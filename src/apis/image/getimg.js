dbb7cf94a6cf4f2d07739052daddc9e502e6bfb4
塞姆贝尔 <554170320@qq.com>
2023-11-17 01:10:01 +0800

    add some api module

---
+++ src/apis/image/getimg.js
@@ -0,0 +1,12 @@
+import request from '../utils/http.js'
+
+
+	
+export  const Getimg = (data,token)=>{
+	// return console.log(data)
+	return request({
+		url: '/api/asset/image/fetch',
+		method: 'post',
+		data
+	})
+}
---
+++ src/apis/image/upload.js
@@ -0,0 +1,12 @@
+import request from '../utils/http.js'
+
+export  const Upload = (data,token)=>{
+	// return console.log(data)
+	return request({
+		url: '/api/asset/image/upload',
+		headers: {
+		      'Authorization': `Bearer ${token}`
+		},
+		data
+	})
+}
